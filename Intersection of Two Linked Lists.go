db95c4e7582d224c094fdfdeb31e4a89bca89309
yashpatil1998 <yashpatil1998@gmail.com>
2023-08-03 23:01:12 +0530

    intersection of 2 linked lists

---
+++ Intersection of Two Linked Lists.go
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+    if headA == nil || headB == nil { 
+        return nil 
+    }
+    a := headA
+    b := headB
+    for a != b {
+        if a == nil {
+            a = headB
+        } else {
+            a = a.Next
+        }
+        if b == nil {
+            b = headA
+        } else {
+            b = b.Next
+        }
+    }
+    return a
+}
