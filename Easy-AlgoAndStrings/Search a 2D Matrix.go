7184b7e23a20bb4b109157478beca9191122c3dd
yashpatil1998 <yashpatil1998@gmail.com>
2023-08-07 22:13:32 +0530

    Easy Matrix Problem

---
+++ Easy-AlgoAndStrings/Search a 2D Matrix.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/search-a-2d-matrix
+
+package EasyAlgoAndStrings
+
+func searchMatrix(matrix [][]int, target int) bool {
+    m := len(matrix)
+    n := len(matrix[0])
+    for i := 0; i < m; i++ {
+        if target <= matrix[i][n-1] && target >= matrix[i][0] {
+            for j := 0; j < n; j++ {
+                if target == matrix[i][j] {
+                    return true
+                }
+            }
+        } else {
+            continue
+        }
+    }
+    return false
+}
